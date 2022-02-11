1b3c82c5f34f2cb900652ab9aaa6dcfabe0bd11c
Crystal <crystallynnvoss@gmail.com>
2022-02-11 22:49:51 +0000

    Fixed all_species and get_villagers_by_species functions

---
+++ villager_data.py
@@ -0,0 +1,117 @@
+"""Functions to parse a file containing villager data."""
+
+def all_species(filename):
+    """Return a set of unique species in the given file.
+
+    Arguments:
+        - filename (str): the path to a data file
+
+    Return:
+        - set[str]: a set of strings
+    """
+    data = open(filename)
+    species = []
+    for line in data: 
+        line = line.strip()
+        raw_profile = line.split("|")
+        animal = raw_profile[1]
+        species.append(animal)
+    species = set(species)
+    return species
+
+
+def get_villagers_by_species(filename, search_string="All"):
+    """Return a list of villagers' names by species.
+
+    Arguments:
+        - filename (str): the path to a data file
+        - search_string (str): optional, the name of a species
+
+    Return:
+        - list[str]: a list of names
+    """
+    data = open(filename)
+    villagers = []
+    for line in data: 
+        line = line.strip()
+        raw_profile = line.split("|")
+        animal = raw_profile[1]
+        name = raw_profile[0]
+        if search_string = "All":
+            villagers.append(name)
+
+        if animal == search_string:
+            villagers.append(name)
+
+    return sorted(villagers)
+
+
+def all_names_by_hobby(filename):
+    """Return a list of lists containing villagers' names, grouped by hobby.
+
+    Arguments:
+        - filename (str): the path to a data file
+
+    Return:
+        - list[list[str]]: a list of lists containing names
+    """
+
+    # TODO: replace this with your code
+
+    return []
+
+
+def all_data(filename):
+    """Return all the data in a file.
+
+    Each line in the file is a tuple of (name, species, personality, hobby,
+    saying).
+
+    Arguments:
+        - filename (str): the path to a data file
+
+    Return:
+        - list[tuple[str]]: a list of tuples containing strings
+    """
+
+    all_data = []
+
+    # TODO: replace this with your code
+
+    return all_data
+
+
+def find_motto(filename, villager_name):
+    """Return the villager's motto.
+
+    Return None if you're not able to find a villager with the
+    given name.
+
+    Arguments:
+        - filename (str): the path to a data file
+        - villager_name (str): a villager's name
+
+    Return:
+        - str: the villager's motto or None
+    """
+
+    # TODO: replace this with your code
+
+
+def find_likeminded_villagers(filename, villager_name):
+    """Return a set of villagers with the same personality as the given villager.
+
+    Arguments:
+        - filename (str): the path to a data file
+        - villager_name (str): a villager's name
+    
+    Return:
+        - set[str]: a set of names
+
+    For example:
+        >>> find_likeminded_villagers('villagers.csv', 'Wendy')
+        {'Bella', ..., 'Carmen'}
+    """
+
+    # TODO: replace this with your code
+file_name.close()
